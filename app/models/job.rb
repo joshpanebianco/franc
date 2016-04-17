a06b6c2594d0fa60a812f0d324bfedd590580c26
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-04-18 09:46:02 +1000

    added some associations, still working on adding migration to user table

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :requirements, presence: true
+  validates :rumuneration, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :applications
+  has_many :jobs
 end

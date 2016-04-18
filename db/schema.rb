c1d46198d20293e602372b3e6267af11a58ee989
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-04-18 14:44:33 +1000

    adde link to homepage, testing style

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160418022106) do
     t.string   "remuneration"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "user_id"
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|

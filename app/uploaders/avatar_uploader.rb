f18dadc9cfc9816090c59b62fdf692892598ee7a
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-04-21 12:18:43 +1000

    fuck you all

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,6 +11,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
    process :eager => true
    process :resize_to_fit => [50, 50]
  end
+
+ 
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
